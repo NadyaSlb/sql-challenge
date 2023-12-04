e97a4d21f17df11a22ee46ff5bcfb776e314a67d
NadyaSlb <nslabuk1@email.cpcc.edu>
2023-12-03 21:15:09 -0500

    queries schema and readme file added

--- EmployeeSQL/Table_schema.sql
+++ EmployeeSQL/Table_schema.sql
@@ -1,36 +1,36 @@
 ﻿CREATE TABLE titles (
-title_id VARCHAR(25) PRIMARY KEY,
-title VARCHAR(255));
+title_id VARCHAR(25) PRIMARY KEY NOT NULL,
+title VARCHAR(255) NOT NULL);
 
 CREATE TABLE employees (
-emp_no INT PRIMARY KEY,
-emp_title_id VARCHAR(25),
+emp_no INT PRIMARY KEY NOT NULL,
+emp_title_id VARCHAR(25) NOT NULL,
 birth_date DATE,
-first_name VARCHAR(255),
-last_name VARCHAR(255),
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
 sex VARCHAR(25),
 hire_date DATE,
 FOREIGN KEY (emp_title_id) REFERENCES titles(title_id));
 
 CREATE TABLE salaries (
-emp_no INT PRIMARY KEY,
+emp_no INT PRIMARY KEY NOT NULL,
 salary INT,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
 CREATE TABLE departments (
-dept_no VARCHAR(25) PRIMARY KEY,
-dept_name VARCHAR(255));
+dept_no VARCHAR(25) PRIMARY KEY NOT NULL,
+dept_name VARCHAR(255) NOT NULL);
 
 CREATE TABLE dept_emp (
-emp_no INT,
-dept_no VARCHAR(25),
+emp_no INT NOT NULL,
+dept_no VARCHAR(25) NOT NULL,
 CONSTRAINT dept_emp_pk PRIMARY KEY (emp_no, dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(25),
-emp_no INT,
+dept_no VARCHAR(25) NOT NULL,
+emp_no INT NOT NULL,
 CONSTRAINT dept_man_pk PRIMARY KEY (dept_no, emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sql-challenge
+
+## Description
+
+This repository contains Entity Relationship Diagram of the tables, the SQL code to create a table schema for the CSV files, and SQL queries for data analysis.
